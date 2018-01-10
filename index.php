d2cd32036ec2a8d9155a66c76b545f5b0759cf4c
zhangzhijian <zhangzhijian@doweidu.com>
2018-01-10 11:02:07 +0800

    修改

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_set('Asia/shanghai'); 
+date_default_timezone_set('Asia/shanghai');
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
